5e9a00476c5ea110471470c555efff9ad8024137
Nathan Schepers <3958483+nathanschepers@users.noreply.github.com>
2026-08-01 13:05:30 +0300

    docstring: functions by role, not by name

--- src/pylongfellow/__init__.py
+++ src/pylongfellow/__init__.py
@@ -1,8 +1,8 @@
 """A Python interface to implementations of Longfellow zero-knowledge mdoc proofs.
 
 [`Pylongfellow`][pylongfellow.Pylongfellow], constructed with an implementation
-(`google-cpp` or `isrg-rust`), provides `generate_circuit`, `load_circuit`, `prove`,
-and `verify`.
+(`google-cpp` or `isrg-rust`), provides functions for circuit generation, proving, and
+verifying.
 """
 
 from importlib.metadata import version
